8e5e6757e78d8a02d61259237c2340c19a5b2bd0
Racci <racci@racci.dev>
2022-02-16 14:17:10 +1100

    chore: initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.6.10"
+}
+
+group = "dev.racci"
+version = "0.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
